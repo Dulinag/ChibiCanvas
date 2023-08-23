b192715657d3798cb85d65dfdc34db98a02806f7
Shashi Sugrim <ssugrim1430@bths.edu>
2023-08-23 19:26:47 -0400

    added queries to help login/create account fns

--- Backend/src/queries.ts
+++ Backend/src/queries.ts
@@ -1,5 +1,10 @@
 const getArtworks = "SELECT * FROM ARTWORKS;";
+const addUser = "INSERT INTO USERS(username, password) VALUES ($1, $2)";
+const checkUserExists = "SELECT * FROM USERS WHERE username=$1";
+
 
 export default {
-    getArtworks
+    getArtworks,
+    addUser,
+    checkUserExists
 }
