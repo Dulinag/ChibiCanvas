bd1f26ea3ba83380ba6156b5a55ce258fb371902
Shashi Sugrim <ssugrim1430@bths.edu>
2023-08-23 19:56:01 -0400

    queries to help getFeatured fn

--- Backend/src/queries.ts
+++ Backend/src/queries.ts
@@ -1,10 +1,13 @@
 const getArtworks = "SELECT * FROM ARTWORKS;";
 const addUser = "INSERT INTO USERS(username, password) VALUES ($1, $2)";
 const checkUserExists = "SELECT * FROM USERS WHERE username=$1";
-
+const getFeatured = "SELECT * FROM FEATURED;";
+const getArtworkByID = "SELECT * FROM ARTWORKS WHERE product_id=$1";
 
 export default {
     getArtworks,
     addUser,
-    checkUserExists
+    checkUserExists,
+    getFeatured,
+    getArtworkByID
 }
