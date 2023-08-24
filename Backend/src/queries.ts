70bf01aaea7f46de4b3c892ef9cad1cbf78b1d14
Shashi Sugrim <ssugrim1430@bths.edu>
2023-08-23 20:28:27 -0400

    helpful queries for add/remove from cart

--- Backend/src/queries.ts
+++ Backend/src/queries.ts
@@ -3,11 +3,21 @@ const addUser = "INSERT INTO USERS(username, password) VALUES ($1, $2)";
 const checkUserExists = "SELECT * FROM USERS WHERE username=$1";
 const getFeatured = "SELECT * FROM FEATURED;";
 const getArtworkByID = "SELECT * FROM ARTWORKS WHERE product_id=$1";
+const checkCart = 'SELECT * FROM (SELECT * FROM CART WHERE username =$1) categories WHERE categories.product_id = $2;'
+const addCart = 'UPDATE CART SET quantity = quantity + $3 WHERE product_id = $2 AND username = $1;';
+const updateCart = 'UPDATE CART SET quantity = quantity - $3 WHERE product_id = $2 AND username = $1;';
+const checkQuantityCart = 'SELECT quantity FROM CART WHERE username=$1 AND product_id=$2;';
+const deleteFromCart = 'DELETE FROM CART WHERE username=$1 AND product_id=$2';
 
 export default {
     getArtworks,
     addUser,
     checkUserExists,
     getFeatured,
-    getArtworkByID
+    getArtworkByID,
+    addCart,
+    checkCart,
+    updateCart,
+    checkQuantityCart,
+    deleteFromCart
 }
