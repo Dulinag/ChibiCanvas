2635a9356666a54dbe0c506e923874b19f25af9b
Shashi Sugrim <ssugrim1430@bths.edu>
2023-08-25 18:03:40 -0400

    removed padding for neater look

--- Frontend/src/Profile.jsx
+++ Frontend/src/Profile.jsx
@@ -227,7 +227,7 @@ Hello        </Typography>
       </Card>
       </Spacer>
     </Container>
-<Padder>h</Padder>
+{/* <Padder>h</Padder> */}
 <ul className='flexcontainer' >
 <ArtworkCard title="Paella" price="$50" date="Aug 2023" imgURL="https://st2.depositphotos.com/1868949/8012/i/450/depositphotos_80126386-stock-photo-spanish-paella-with-seafood.jpg" description="This is an impressive paella"/>
 {artworks? artworks.map((data, index) => {
